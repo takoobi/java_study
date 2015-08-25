113c11eebf5b044a3f6d40afb9caaf53f4b18d06
sutlige <sutlige@gmail.com>
2015-08-25 14:09:50 +0900

    no message

---
+++ src/main/java/com/board/BoardController.java
@@ -0,0 +1,266 @@
+package com.board;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.model.BoardBean;
+import com.model.ReplyBean;
+
+
+@Controller
+@RequestMapping(value="board")
+public class BoardController {
+
+	@Autowired
+	private BoardDAO boardDao;
+	
+	String category="";
+	
+	//글쓰기
+	@RequestMapping("/write")
+	public String boardWrite(){
+		return "board/board_write";
+	}
+	
+	//글쓰기 액션
+	@RequestMapping("/writeAction")
+	public String boardWriteAction(MultipartHttpServletRequest request, HttpSession session) throws IOException{
+		
+		MultipartFile multipartFile = request.getFile("image");
+		if(!multipartFile.isEmpty()){
+			File file = new File("C:\\Users\\kimkoonho\\Desktop\\uploadtest", multipartFile.getOriginalFilename());
+			multipartFile.transferTo(file);
+		}
+		
+		BoardBean bean = new BoardBean();
+		bean.setTitle(request.getParameter("title"));
+		bean.setTag(request.getParameter("tag"));
+		bean.setEmail((String) session.getAttribute("email"));
+		bean.setImage(multipartFile.getOriginalFilename());
+		bean.setDescription(request.getParameter("description"));
+		bean.setCategory(request.getParameter("category"));
+		
+		boardDao.insert(bean);
+		
+		return "redirect:"+request.getParameter("category");
+	}
+
+	//글 목록화면
+	@RequestMapping("/{category}")
+	public String list(@PathVariable("category") String category, Model model){
+		model.addAttribute("category", category);
+		return "board/board_list";
+	}
+	
+	//태그불러오기
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value="/doList")
+	public void doList(HttpServletRequest request, HttpServletResponse response){
+		String result = request.getParameter("result");
+		category = request.getParameter("category");
+		int num = Integer.parseInt(request.getParameter("num"));
+		int startNum = (num-1)*8+1;
+		int endNum = startNum+7;
+		List<BoardBean> list = new ArrayList<BoardBean>();
+		String nickname=null;
+		
+        list = boardDao.pagingList(category, result, startNum, endNum);
+		JSONObject jsonObject = new JSONObject();
+		String json="[";
+		for(int i=0; i<list.size(); i++){
+			BoardBean boardBean = (BoardBean) list.get(i);
+			
+			//닉네임 가져오기
+			nickname = boardDao.getNickname(boardBean.getEmail());
+					
+			json+="{\"pk\":\""+boardBean.getPk()+"\","
+					+ "\"email\":\""+boardBean.getEmail()+"\","
+					+ "\"title\":\""+boardBean.getTitle()+"\","
+					+ "\"description\":\""+boardBean.getDescription()+"\","
+					+ "\"tag\":\""+boardBean.getTag()+"\","
+					+ "\"image\":\""+boardBean.getImage()+"\","
+					+ "\"good\":\""+boardBean.getGood()+"\","
+					+ "\"bad\":\""+boardBean.getBad()+"\","
+					+ "\"hit\":\""+boardBean.getHit()+"\","
+					+ "\"create_date\":\""+boardBean.getCreate_date()+"\","
+					+ "\"nickname\":\""+nickname+"\","
+					+ "\"category\":\""+category+"\"}";
+		    if( i != list.size() -1){
+		        json += ",";
+		    }
+		}
+		json += "]";
+		jsonObject.put("list", json);
+		//System.out.println(jsonObject);
+
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out; 
+		try{
+			out = response.getWriter();
+			out.print(jsonObject.toJSONString());
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+
+	//글 상세보기
+	@RequestMapping("/board_view")
+	public ModelAndView board_view(HttpServletRequest request,HttpSession session){
+		int pk = Integer.parseInt(request.getParameter("pk"));
+		String category = request.getParameter("category");
+		String email = (String)session.getAttribute("email");
+		Boolean good_id_check;
+		Boolean bad_id_check;
+		
+		ModelAndView mav= new ModelAndView();
+		mav.setViewName("board/board_view");
+		
+		
+		
+		BoardBean boardBean = boardDao.one_board(category, pk);
+		String nickname = boardDao.getNickname(boardBean.getEmail());
+		mav.addObject("boardBean", boardBean);
+		mav.addObject("nickname", nickname);
+		
+		if(email!=null){
+			if(boardBean.getGood_id()!=null){
+				good_id_check=boardBean.getGood_id().contains(email);
+			}else{
+				good_id_check=false;
+			}
+			if(boardBean.getBad_id()!=null){
+				bad_id_check=boardBean.getBad_id().contains(email);
+			}else{
+				bad_id_check=false;
+			}
+			mav.addObject("good_id_check", good_id_check);
+			mav.addObject("bad_id_check", bad_id_check);
+		}
+		
+		return mav;
+	}
+
+	//공감, 공감취소, 비공감, 비공감취소
+	@RequestMapping("/emotion")
+	public @ResponseBody Map<String, Object> emotion(HttpServletRequest request, HttpSession session){
+		int number = Integer.parseInt(request.getParameter("number"));
+		int pk = Integer.parseInt(request.getParameter("pk"));
+		category=request.getParameter("category");
+		String email=(String) session.getAttribute("email");
+
+		//1-공감,2-공감취소,3-비공감,4-비공감취소		
+		boardDao.emotion(category, number, pk, email);
+		Map<String, Object> map = new HashMap<String, Object>();	
+		map = boardDao.emotion_count_get(category, pk);
+
+		String good_id = null;
+		String bad_id=null;
+		Boolean good_id_check=null;
+		Boolean bad_id_check=null;
+		
+		if(map!=null){
+			if(map.get("good_id")!=null){
+				good_id = (String) map.get("good_id");
+				good_id_check = good_id.contains(email);
+			}else {
+				good_id_check=false;
+			}
+			if(map.get("bad_id")!=null){
+				bad_id = (String) map.get("bad_id");					
+				bad_id_check = bad_id.contains(email);				
+			}else{
+				bad_id_check=false;			
+			}			
+		}
+		
+		map.remove("good_id");
+		map.remove("bad_id");
+		map.put("good_id_check", good_id_check);
+		map.put("bad_id_check", bad_id_check);
+		return map;
+	}
+	
+	//댓글 추가하기
+	@RequestMapping("/reply")
+	public @ResponseBody List<ReplyBean> reply(HttpServletRequest request, HttpSession session){
+		String description = request.getParameter("description");
+		String board_key = request.getParameter("board_key");
+		String email = (String)session.getAttribute("email");
+		
+		if(description!=null){
+			boardDao.reply_insert(description, board_key, email);
+		}
+		List<ReplyBean> list = boardDao.reply_getData(board_key);
+
+		return list;
+	}
+	
+	//광장
+	@RequestMapping("/allList")
+	public @ResponseBody Map<String, Object> allList(){
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		List<BoardBean> list=boardDao.get_setCount_board("cafe");
+		for (BoardBean boardBean : list) {
+			boardBean.setCategory("cafe");
+		}
+		map.put("cafe", list);
+    	list=boardDao.get_setCount_board("bar");
+		map.put("bar", list);
+		for (BoardBean boardBean : list) {
+			boardBean.setCategory("bar");
+		}
+		list=boardDao.get_setCount_board("school");
+		map.put("school", list);
+		for (BoardBean boardBean : list) {
+			boardBean.setCategory("school");
+		}
+		list=boardDao.get_setCount_board("penthouse");
+		map.put("penthouse", list);
+		for (BoardBean boardBean : list) {
+			boardBean.setCategory("penthouse");
+		}
+		list=boardDao.get_setCount_board("military");
+		map.put("military", list);
+		for (BoardBean boardBean : list) {
+			boardBean.setCategory("military");
+		}
+		list=boardDao.get_setCount_board("exile");
+		map.put("exile", list);
+		for (BoardBean boardBean : list) {
+			boardBean.setCategory("exile");
+		}
+		list=boardDao.get_setCount_board("music");
+		map.put("music", list);
+		for (BoardBean boardBean : list) {
+			boardBean.setCategory("music");
+		}
+		list=boardDao.get_setCount_board("broadcast");
+		map.put("broadcast", list);
+		for (BoardBean boardBean : list) {
+			boardBean.setCategory("broadcast");
+		}
+		
+		return map;
+	}
+}
---
+++ src/main/java/com/member/MemberController.java
@@ -0,0 +1,296 @@
+package com.member;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import com.model.MemberBean;
+
+@Controller
+@RequestMapping(value="member")
+public class MemberController {
+
+	@Autowired
+	private MemberDAO memberDao;
+	
+	//회원가입화면
+	@RequestMapping(value="/signupPage")
+	public String signupPage(){
+		return "member/register";
+	}
+	//회원가입화면
+	@RequestMapping(value="/add")
+	public String add(){
+		return "member/member_add";
+	}
+
+	//이메일중복확인
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value="/emailCheckAction")
+	public void emailCheckAction(HttpServletRequest request, HttpServletResponse response) throws IOException{
+		String email = request.getParameter("email");
+		
+		boolean result = memberDao.emailCheck(email);
+		PrintWriter out=response.getWriter();
+		
+		JSONObject ob =new JSONObject();
+	
+		ob.put("result", result);
+
+		out.print(ob);
+	}
+	
+
+	
+	//닉네임중복확인
+	@SuppressWarnings("unchecked")
+	@RequestMapping(value="/nicknameCheckAction")
+	public void nicknameCheckAction(HttpServletRequest request, HttpServletResponse response) throws IOException{
+		String nickname = request.getParameter("nickname");
+		
+		boolean result = memberDao.nicknameCheck(nickname);
+		PrintWriter out=response.getWriter();
+		
+		JSONObject ob =new JSONObject();
+
+		ob.put("result", result);
+		
+		out.print(ob);
+	}
+
+	//회원추가하기
+	@RequestMapping(value="/addAction")
+	public String addAction(HttpServletRequest request, HttpServletResponse response) throws IllegalStateException, IOException{
+		
+		MemberBean bean = new MemberBean();
+		bean.setEmail(request.getParameter("email"));
+		bean.setPw(request.getParameter("pw"));
+		bean.setNickname(request.getParameter("nickname"));
+		bean.setTitle(request.getParameter("title"));
+		
+		memberDao.insert(bean);
+		
+		PrintWriter out=response.getWriter();  
+		
+		response.setContentType("text/html; charset=utf-8");
+        out.println("<script>");
+        out.println("alert('회원가입이 완료되었습니다.');");
+        out.println("location.href='http://localhost:8088/LOVE/index.jsp'");
+        out.println("</script>");
+        out.close();   
+        
+        return null;
+		//return "redirect:/index.jsp";
+	}
+	
+	//회원정보수정화면
+	@RequestMapping(value="/modify")
+	public String modify(HttpServletRequest request, HttpServletResponse response,HttpSession session) throws UnsupportedEncodingException{
+		request.setCharacterEncoding("utf-8");
+		
+		MemberBean one_data = new MemberBean();
+		
+		one_data=memberDao.getOneData((String)session.getAttribute("email"));
+		
+		if(one_data==null){
+			return "redirect:/url.jsp";
+		}
+		
+		request.setAttribute("OneData", one_data);
+		
+		return "member/member_modify";
+	}
+	
+	//회원정보수정하기
+	@RequestMapping(value="/modifyAction")
+	public String modifyAction(MultipartHttpServletRequest request,HttpServletResponse response) throws IllegalStateException, IOException{
+		//선언 및 생성
+		MultipartFile multipartFile = request.getFile("image");
+		MemberBean bean = new MemberBean();
+		String savePath = request.getSession().getServletContext().getRealPath("/")+"resources/image/profile";
+		
+		//조건 처리
+		if(!multipartFile.isEmpty()){
+			File file = new File(savePath, multipartFile.getOriginalFilename());
+			multipartFile.transferTo(file);
+			bean.setImage(multipartFile.getOriginalFilename());
+		} else{
+			bean.setImage("original_image.jpg");
+		}
+		
+		//값 넣기
+		bean.setEmail(request.getParameter("email"));
+		bean.setPw(request.getParameter("pw"));
+		bean.setNickname(request.getParameter("nickname"));
+		bean.setDescription(request.getParameter("description"));
+		bean.setGender(request.getParameter("gender"));
+		
+		//db처리
+		memberDao.update(bean);
+		
+		//java파일에서 자바스크립트 사용하기
+        response.setContentType("text/html; charset=utf-8");
+        PrintWriter out=response.getWriter();	  
+        out.println("<script>");
+        out.println("alert('수정했습니다.');");
+        out.println("location.href='http://localhost:8088/LOVE/index.jsp'");
+        out.println("</script>");
+		return null;		
+	}
+	//회원탈퇴화면
+	@RequestMapping(value="/delete")
+	public String delete(){
+		return "member/member_delete";
+	}
+	//회원탈퇴하기
+	@RequestMapping(value="/deleteAction")
+	public @ResponseBody Map<String, String> deleteAction(HttpServletRequest request, HttpServletResponse response,HttpSession session){
+		//선언 및 생성
+		Map<String, String> map = new HashMap<String,String>();
+		String email=(String)session.getAttribute("email");
+		String pw=request.getParameter("inputPw");
+		
+		//db처리
+		int check=memberDao.delete(email,pw);
+		
+		//조건 처리
+		if(check==1){
+			session.invalidate();
+			map.put("result", "ok");
+		}else{
+			map.put("result", "no");
+		}
+				
+		return map;
+	}
+	
+	//로그인화면
+	@RequestMapping(value="/login")
+	public String login(){
+		return "member/member_login";
+	}
+	//로그인하기
+	@RequestMapping(value="/loginAction")
+	public @ResponseBody Map<String, String> loginAction(HttpServletRequest request, HttpServletResponse response, HttpSession session) throws IOException{
+		//선언 및 생성
+		Map<String, String> map = new HashMap<String, String>();
+		String email=request.getParameter("email");
+		String pw=request.getParameter("pw");
+
+		//db처리
+		int check=memberDao.loginCheck(email,pw);
+		
+		//조건 생성
+		if(check==1){
+			String nickname = memberDao.getNickname(email);
+			session.setAttribute("email", email);
+			session.setAttribute("nickname", nickname);
+			map.put("result", "ok");
+		}else if(check==0){
+			map.put("result", "noPw");
+		}else{
+			map.put("result", "noId");
+		}
+		return map;
+	}
+	
+	//로그아웃하기
+	@RequestMapping(value="/logout")
+	public String logout(HttpSession session){
+		session.invalidate();
+		return "redirect:/index.jsp";
+	}
+	
+	//비밀번호찾기화면
+	@RequestMapping(value="/find")
+	public String find(){
+		return "member/member_find";
+	}
+	//비밀번호찾기
+	@RequestMapping(value="/findAction")
+	public @ResponseBody Map<String, String> findAction(HttpServletRequest request, HttpServletResponse response) throws AddressException, MessagingException, IOException{
+		//선언 및 생성
+		Map<String, String> map = new HashMap<String, String>();
+		
+		//받을 값
+		String email=request.getParameter("email");
+		
+		//db처리
+		String result=memberDao.find(email);
+		
+		//조건 생성
+		if(!result.equals("무존재")){
+	        //난수 발생
+	        int temporaryPassword=0;
+	        temporaryPassword=(int)(Math.random()*90000)+10000; // 10000~99999
+	                
+	        // 메일 관련 정보
+	        String host = "smtp.gmail.com";
+	        String username = "sbv20000@gmail.com";
+	        String password = "rlarnsgh91";
+	          
+	        // 메일 내용
+	        String recipient = email;
+	        String subject = "임시비밀번호 입니다!";
+	        String body = String.valueOf(temporaryPassword);
+	          
+	        //properties 설정
+	        Properties props = new Properties();
+	        props.put("mail.smtps.auth", "true");
+	        
+	        //임시비밀번호 db처리
+	        memberDao.temporaryPassword(email, temporaryPassword);
+	        
+	        // 메일 세션
+	        Session session = Session.getDefaultInstance(props);
+	        MimeMessage msg = new MimeMessage(session);
+	      
+	        // 메일 관련
+	        msg.setSubject(subject);
+	        msg.setText(body);
+	        msg.setFrom(new InternetAddress(username));
+	        msg.addRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+	      
+	        // 발송 처리
+	        Transport transport = session.getTransport("smtps");
+	        transport.connect(host, username, password);
+	        transport.sendMessage(msg, msg.getAllRecipients());
+	        transport.close();
+	        
+	        // 임시 비밀번호 발송 팝업
+	        String[] str = email.split("@");            
+            String emailResult = str[1];     
+
+	        //보낼 데이터 담기
+	        map.put("result", "ok");
+	        map.put("emailResult", emailResult);
+	        return map;
+		} else{
+			map.put("result", "no");
+			return map;
+		}
+	}
+}
